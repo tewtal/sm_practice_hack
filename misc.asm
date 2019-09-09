80a50a48f2644bfb0f757f2bb0d11151bfece7a2
Helge Milde <helge@monsternett.no>
2019-09-09 14:31:00 +0200

    Remove delay on Samus after a teleport

---
+++ misc.asm
@@ -0,0 +1,17 @@
+; Skips the waiting time after teleporting
+org $90E870
+    JMP $E898
+
+; Skips fade in when teleporting
+org $828106
+    JSL hook_after_teleport
+    NOP : NOP
+
+org $DFDA00
+hook_after_teleport:
+    LDA #$0008
+    STA $0998 
+    SEP #$20
+    LDA #$0F : STA $51
+    REP #$20
+    RTL
