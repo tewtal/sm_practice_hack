5224fa9251026b756b7e096aa5f3d569f8d5e75a
InsaneFirebat <InsaneFirebat@gmail.com>
2020-01-06 03:38:36 -0500

    Copy/paste mistake in filename

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ cp resources/sm_orig.sfc build/smhack20_rbo.sfc && cd src && ..\tools\asar --no-
 
 cp resources/sm_orig.sfc build/smhack20_kpdr25.sfc && cd src && ..\tools\asar --no-title-check -DFEATURE_SD2SNES=0 -DCATEGORY=2 main.asm ../build/smhack20_kpdr25.sfc && cd ..
 
-cp resources/sm_orig.sfc build/smhack20_gtclassic.sfc && cd src && ..\tools\asar --no-title-check -DFEATURE_SD2SNES=0 -DCATEGORY=3 main.asm ../build/smhack20_kpdr25.sfc && cd ..
+cp resources/sm_orig.sfc build/smhack20_gtclassic.sfc && cd src && ..\tools\asar --no-title-check -DFEATURE_SD2SNES=0 -DCATEGORY=3 main.asm ../build/smhack20_gtclassic.sfc && cd ..
 
 cp resources/sm_orig.sfc build/smhack20_sd2snes.sfc && cd src && ..\tools\asar --no-title-check -DFEATURE_SD2SNES=1 -DCATEGORY=0 main.asm ../build/smhack20_sd2snes.sfc && cd ..
 
