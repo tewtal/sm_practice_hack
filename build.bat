d9a7051a12c6a806bf35906780dea8afbf41586a
Thomas Backmark <total@viskos.org>
2020-05-31 02:40:48 +0200

    Fix build.bat

--- build.bat
+++ build.bat
@@ -1,20 +1,20 @@
-#!/bin/bash
+@echo off
 
-echo "Building SM Practice Hack"
+echo Building SM Practice Hack
 cd resources
-python3 create_dummies.py 00.sfc ff.sfc
+python create_dummies.py 00.sfc ff.sfc
 
-echo "Building saveless version"
+echo Building saveless version
 copy *.sfc ..\build
 ..\tools\asar.exe --no-title-check -DFEATURE_SD2SNES=0 ..\src\main.asm ..\build\00.sfc
 ..\tools\asar.exe --no-title-check -DFEATURE_SD2SNES=0 ..\src\main.asm ..\build\ff.sfc
-python3 create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\smhack20.ips
+python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\smhack20.ips
 
-echo "Building SD2SNES version"
+echo Building SD2SNES version
 copy *.sfc ..\build
 ..\tools\asar --no-title-check -DFEATURE_SD2SNES=1 ..\src\main.asm ..\build\00.sfc
 ..\tools\asar --no-title-check -DFEATURE_SD2SNES=1 ..\src\main.asm ..\build\ff.sfc
-python3 create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\smhack20_sd2snes.ips
+python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\smhack20_sd2snes.ips
 
 del 00.sfc ff.sfc ..\build\00.sfc ..\build\ff.sfc
 cd ..
