c225c8e768ff77262bc66ea0a6825b85d6f0b7b8
InsaneFirebat <InsaneFirebat@gmail.com>
2020-07-25 03:48:48 -0400

    Fix build check
    Revert version number change in build script filenames.

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ echo "Building saveless version"
 cp *.sfc ../build
 ../tools/asar --no-title-check -DFEATURE_SD2SNES=0 ../src/main.asm ../build/00.sfc
 ../tools/asar --no-title-check -DFEATURE_SD2SNES=0 ../src/main.asm ../build/ff.sfc
-python3 create_ips.py ../build/00.sfc ../build/ff.sfc ../build/smhack21.ips
+python3 create_ips.py ../build/00.sfc ../build/ff.sfc ../build/smhack20.ips
 
 echo "Building SD2SNES version"
 cp *.sfc ../build
 ../tools/asar --no-title-check -DFEATURE_SD2SNES=1 ../src/main.asm ../build/00.sfc
 ../tools/asar --no-title-check -DFEATURE_SD2SNES=1 ../src/main.asm ../build/ff.sfc
-python3 create_ips.py ../build/00.sfc ../build/ff.sfc ../build/smhack21_sd2snes.ips
+python3 create_ips.py ../build/00.sfc ../build/ff.sfc ../build/smhack20_sd2snes.ips
 
 rm 00.sfc ff.sfc ../build/00.sfc ../build/ff.sfc
 cd ..
