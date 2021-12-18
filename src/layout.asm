4014d969f39086946c7ab4ea2e0ff06ec58f6b75
idlechild <>
2021-12-17 19:31:54 -0600

    Area Rando patch (Caterpillar Room)

--- src/layout.asm
+++ src/layout.asm
@@ -1,4 +1,17 @@
 
+; Caterpillar middle-left door asm pointer
+org $839094
+    ; Use same asm as elevator door, freeing up asm at $BE1A
+    dw $BA21
+
+; Caterpillar top-left door asm pointer
+org $8390E8
+    dw #layout_asm_caterpillar_no_scrolls
+
+; Caterpillar near-right door asm pointer
+org $839274
+    dw #layout_asm_caterpillar_no_scrolls
+
 ; Green Hill Zone top-left door asm pointer
 org $838DF4
     dw #layout_asm_greenhillzone
@@ -24,6 +37,17 @@ org $83AB92
     dw #layout_asm_magnetstairs
 
 
+; Caterpillar elevator and middle-left door asm
+org $8FBA26
+    ; Replace STA with jump to STA
+    JMP layout_asm_caterpillar_update_scrolls
+
+; Caterpillar bottom-left door asm
+org $8FBE18
+    ; Overwrite PLP : RTS with jump
+    ; Okay to overwrite $BE1A since we freed up that space
+    JMP layout_asm_caterpillar_after_scrolls
+
 ; Ceres Ridley modified state check to support presets
 org $8FE0C0
     dw layout_asm_ceres_ridley_room_state_check
@@ -32,6 +56,12 @@ org $8FE0C0
 org $8FE0DF
     dw layout_asm_ceres_ridley_room_no_timer
 
+; Caterpillar far-right door asm
+org $8FE370
+    ; Optimize existing logic by one byte
+    INC : STA $7ECD2A
+    ; Overwrite extra byte : PLP : RTS with jump
+    JMP layout_asm_caterpillar_after_scrolls
 
 
 org $8FEA00 ; free space for door asm
@@ -113,6 +143,52 @@ layout_asm_greenhillzone:
 
     PLP
   .done
+    RTS
+}
+
+layout_asm_caterpillar_no_scrolls:
+{
+    LDA !ram_cm_arearando : BEQ .done
+    PHP : %a8()
+    BRA layout_asm_caterpillar_after_scrolls
+
+  .done
+    RTS
+}
+
+layout_asm_caterpillar_update_scrolls:
+{
+    STA $7ECD26
+}
+
+layout_asm_caterpillar_after_scrolls:
+{
+    LDA !ram_cm_arearando : BEQ .done
+    %a16()
+
+    ; Decorate gap with blocks
+    LDA #$8562 : STA $7F145E : STA $7F1460 : STA $7F151E : STA $7F1520
+
+    ; Fix wall decoration below blocks
+    LDA #$8543 : STA $7F157E : LDA #$8522 : STA $7F1580
+
+    ; Create visible gap in wall
+    LDA #$00FF : STA $7F14BE : STA $7F14C0
+
+    ; Remove gate and block next to gate
+    STA $7F142C : STA $7F142E : STA $7F1430
+    STA $7F148E : STA $7F14EE : STA $7F154E : STA $7F15AE
+
+    ; Clear gate PLMs and projectiles
+    LDA #$0000 : STA $1C7B : STA $1C7D : STA $19B9
+
+    ; Normal BTS for gate tiles
+    %a8()
+    LDA #$00 : STA $7F6E17 : STA $7F6E18 : STA $7F6E19
+    STA $7F6E48 : STA $7F6E78 : STA $7F6EA8 : STA $7F6ED8
+
+  .done
+    PLP
     RTS
 }
 
