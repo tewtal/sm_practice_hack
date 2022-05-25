c4f6adf62e0e013e3ffcac419938866295f41ce1
Thomas Backmark <total@viskos.org>
2022-05-25 11:30:13 +0200

    Updated build scripts for a third option

--- build.bat
+++ build.bat
@@ -6,8 +6,8 @@ python create_dummies.py 00.sfc ff.sfc
 
 echo Building saveless version
 copy *.sfc ..\build
-..\tools\asar.exe --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\00.sfc
-..\tools\asar.exe --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\ff.sfc
+..\tools\asar --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\00.sfc
+..\tools\asar --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\ff.sfc
 python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\smhack20.ips
 
 echo Building SD2SNES version
