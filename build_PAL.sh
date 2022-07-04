941b4fed40664d26fee4459d6ff585fb4d72a082
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2022-07-04 00:46:08 -0700

    add error trap to build_PAL.sh as well

--- build_PAL.sh
+++ build_PAL.sh
@@ -4,6 +4,10 @@ echo "Building SM PAL Practice Hack"
 cd resources
 python3 create_dummies.py 00.sfc ff.sfc
 
+# Return an error if any command fails.
+success=0
+trap success=1 ERR
+
 echo "Building saveless version"
 cp *.sfc ../build
 ../tools/asar --no-title-check -DFEATURE_SD2SNES=0 -DFEATURE_PAL=1 "$@" ../src/main.asm ../build/00.sfc
@@ -18,3 +22,5 @@ python3 create_ips.py ../build/00.sfc ../build/ff.sfc ../build/smpalhack20_sd2sn
 
 rm 00.sfc ff.sfc ../build/00.sfc ../build/ff.sfc
 cd ..
+
+exit $success
