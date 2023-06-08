1c911a7acd452b3f897549cc7bc94272e033e855
InsaneFirebat <InsaneFirebat@gmail.com>
2023-06-08 10:43:24 -0400

    Save prints/errors to build/print_pc.log
    by p4plus2

--- build_dev.bat
+++ build_dev.bat
@@ -9,7 +9,8 @@ python create_dummies.py 00.sfc ff.sfc
 echo Building SD2SNES Dev version
 copy *.sfc ..\build
 ..\tools\asar --no-title-check --symbols=wla --symbols-path=..\build\Debugging_Symbols.sym -DFEATURE_SD2SNES=1 -DFEATURE_DEV=1 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\00.sfc
-..\tools\asar --no-title-check --symbols=wla --symbols-path=..\build\Debugging_Symbols.sym -DFEATURE_SD2SNES=1 -DFEATURE_DEV=1 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\ff.sfc
+..\tools\asar --no-title-check --symbols=wla --symbols-path=..\build\Debugging_Symbols.sym -DFEATURE_SD2SNES=1 -DFEATURE_DEV=1 -DFEATURE_PAL=0 -DFEATURE_TINYSTATES=0 ..\src\main.asm ..\build\ff.sfc > ..\build\print_pc.log 2>&1
+type ..\build\print_pc.log
 python dos_to_unix.py ..\build\Debugging_Symbols.sym
 python sort_debug_symbols.py ..\build\Debugging_Symbols.sym ..\build\Debugging_Sorted.sym ..\build\Debugging_Combined.sym
 python create_ips.py ..\build\00.sfc ..\build\ff.sfc ..\build\smhack20_sd2snes_dev.ips
